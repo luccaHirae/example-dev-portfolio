6fc3ecdd2de73122c94bddcc95e141296f023ff0
Bruno Hirae <brunodeluca117@outlook.com>
2025-01-28 14:09:14 -0300

    feat: add archivo font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,18 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "var(--font-archivo)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
 export default config;
