276e2689c1507351e6639118eb8b304cb7a03d48
Bruno Hirae <brunodeluca117@outlook.com>
2025-01-31 13:35:05 -0300

    feat: add header animations

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,7 +1,9 @@
-import { Button } from "@/components/Button";
-import { FC } from "react";
+"use client";
 
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+import { Button } from "@/components/Button";
+import { FC, useEffect, useState } from "react";
+import { motion, useAnimate } from "motion/react";
+
 const navItems = [
   {
     label: "About",
@@ -26,35 +28,203 @@ const navItems = [
 ];
 
 const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [topLineScope, topLineAnimate] = useAnimate();
+  const [bottomLineScope, bottomLineAnimate] = useAnimate();
+  const [navScope, navAnimate] = useAnimate();
+
+  useEffect(() => {
+    if (isOpen) {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            translateY: 4,
+          },
+        ],
+        [
+          topLineScope.current,
+          {
+            rotate: 45,
+          },
+        ],
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            translateY: -4,
+          },
+        ],
+        [
+          bottomLineScope.current,
+          {
+            rotate: -45,
+          },
+        ],
+      ]);
+
+      navAnimate(
+        navScope.current,
+        {
+          height: "100%",
+        },
+        {
+          duration: 0.7,
+        }
+      );
+    } else {
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            translateY: 0,
+          },
+        ],
+        [
+          topLineScope.current,
+          {
+            rotate: 0,
+          },
+        ],
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            translateY: 0,
+          },
+        ],
+        [
+          bottomLineScope.current,
+          {
+            rotate: 0,
+          },
+        ],
+      ]);
+
+      navAnimate(
+        navScope.current,
+        {
+          height: 0,
+        },
+        {
+          duration: 0.7,
+        }
+      );
+    }
+  }, [
+    bottomLineAnimate,
+    bottomLineScope,
+    isOpen,
+    navAnimate,
+    navScope,
+    topLineAnimate,
+    topLineScope,
+  ]);
+
   return (
-    <header className="fixed top-0 left-0 w-full backdrop-blur-md">
-      <div className="container !max-w-full">
-        <div className="flex justify-between h-20 items-center">
-          <div>
-            <a href="/">
-              <span className="text-xl font-bold uppercase">
-                Alex&nbsp; Taylor
-              </span>
+    <header>
+      <div
+        ref={navScope}
+        className="fixed top-0 left-0 w-full h-0 overflow-hidden bg-stone-900"
+      >
+        <nav className="mt-20 flex flex-col">
+          {navItems.map(({ href, label }) => (
+            <a
+              href={href}
+              key={label}
+              className="text-stone-200 border-t last:border-b border-stone-800 py-8 group/nav-item relative isolate"
+            >
+              <div className="container !max-w-full flex items-center justify-between">
+                <span className="text-3xl group-hover/nav-item:pl-4 transition-all duration-500">
+                  {label}
+                </span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                  />
+                </svg>
+              </div>
+
+              <div className="absolute bottom-0 -z-10 w-full h-0 bg-stone-800 group-hover/nav-item:h-full transition-all duration-500"></div>
             </a>
-          </div>
+          ))}
+        </nav>
+      </div>
 
-          <div className="flex items-center gap-4">
-            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect x="3" y="15" width="18" height="2" fill="currentColor" />
-                <rect x="3" y="7" width="18" height="2" fill="currentColor" />
-              </svg>
+      <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md">
+        <div className="container !max-w-full">
+          <div className="flex justify-between h-20 items-center">
+            <div>
+              <a href="/">
+                <span className="text-xl font-bold uppercase text-white">
+                  Alex&nbsp; Taylor
+                </span>
+              </a>
             </div>
+          </div>
+        </div>
+      </div>
 
-            <Button variant="primary" className="hidden md:inline-flex">
-              Contact Me
-            </Button>
+      <div className="fixed top-0 left-0 w-full">
+        <div className="container !max-w-full">
+          <div className="flex justify-end h-20 items-center">
+            <div className="flex items-center gap-4">
+              <div
+                onClick={() => setIsOpen((prev) => !prev)}
+                className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <motion.rect
+                    x="3"
+                    y="7"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                    ref={topLineScope}
+                    style={{
+                      transformOrigin: "12px 8px", // center of the rectangle
+                      // transform: "translateY(4px) rotate(45deg)",
+                    }}
+                  />
+                  <motion.rect
+                    x="3"
+                    y="15"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                    ref={bottomLineScope}
+                    style={{
+                      transformOrigin: "12px 16px", // center of the rectangle
+                      // transform: "translateY(-4px) rotate(-45deg)",
+                    }}
+                  />
+                </svg>
+              </div>
+
+              <Button variant="primary" className="hidden md:inline-flex">
+                Contact Me
+              </Button>
+            </div>
           </div>
         </div>
       </div>
