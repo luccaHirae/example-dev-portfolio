29221ef14ae9948f6379ab9a01a6c023a23e9507
Bruno Hirae <brunodeluca117@outlook.com>
2025-02-03 11:53:12 -0300

    feat: add footer section animations

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,43 +1,72 @@
+"use client";
+
 import { Button } from "@/components/Button";
-import { FC } from "react";
+import { useTextRevealAnimation } from "@/hooks/useTextRevealAnimation";
+import { useInView } from "motion/react";
+import { FC, MouseEvent, useEffect } from "react";
 
 const navItems = [
   {
-    href: "#",
+    href: "#home",
     label: "Home",
   },
   {
-    href: "#",
+    href: "#projects",
     label: "Projects",
   },
   {
-    href: "#",
+    href: "#testimonials",
     label: "Testimonials",
   },
   {
-    href: "#",
+    href: "#faqs",
     label: "Faqs",
   },
   {
-    href: "#",
+    href: "#contact",
     label: "Contact",
   },
 ];
 
 const Footer: FC = () => {
+  const { scope, entranceAnimation } = useTextRevealAnimation();
+  const inView = useInView(scope);
+
+  useEffect(() => {
+    if (inView) entranceAnimation();
+  }, [entranceAnimation, inView]);
+
+  const handleClickNavItem = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+
+    const url = new URL(event.currentTarget.href);
+    const hash = url.hash;
+
+    const target = document.querySelector(hash);
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <footer className="bg-stone-900 text-white" id="contact">
       <div className="container">
         <div className="section">
           <div className="flex items-center gap-3">
-            <div className="size-3 rounded-full bg-green-400"></div>
+            <div className="size-3 rounded-full bg-green-400 animate-pulse"></div>
 
             <span className="uppercase">One spot available for next month</span>
           </div>
 
           <div className="grid md:grid-cols-3 md:items-center">
             <div className="md:col-span-2">
-              <h2 className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight">
+              <h2
+                ref={scope}
+                className="text-4xl md:text-7xl lg:text-8xl mt-8 font-extralight"
+              >
                 Enough talk. Let&apos;s make something great together
               </h2>
 
@@ -45,20 +74,39 @@ const Footer: FC = () => {
                 variant="secondary"
                 className="mt-8"
                 iconAfter={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
+                  <div className="size-6 overflow-hidden">
+                    <div className="w-12 h-6 flex transition-transform duration-300 group-hover/button:-translate-x-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 }
               >
                 info@alextaylor.com
@@ -68,7 +116,7 @@ const Footer: FC = () => {
             <div>
               <nav className="flex flex-col md:items-end gap-8 mt-16 md:mt-0">
                 {navItems.map(({ href, label }) => (
-                  <a href={href} key={label}>
+                  <a href={href} key={label} onClick={handleClickNavItem}>
                     <Button variant="text" className="text-lg">
                       {label}
                     </Button>
